f72e894c6535f058e66888bfbafb502e360da0ff
Mathias <mathias.lafayedemicheaux@gmail.com>
2026-02-18 01:38:17 +0900

    added doors, broken teleport though

--- Godot/game-jam/player.gd
+++ Godot/game-jam/player.gd
@@ -9,13 +9,13 @@ const RUN_FACTOR = 2
 
 @onready var head: Node3D = $Head
 @onready var camera: Camera3D = $Head/Camera3D
+@onready var raycast: RayCast3D = $Head/RayCast3D
 
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _unhandled_input(event: InputEvent) -> void:
-	
 	# Change mouse capture mode with escape
 	if event.is_action_released("Escape"):
 		toggle_mouse_mode()
@@ -23,20 +23,29 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 		rotate_head(event)
 
-	
 
 
 func _physics_process(delta: float) -> void:
+	movement(delta)
+	if raycast.is_colliding():
+		$UI/Center/Crosshair.rotation = PI/4
+		var col: = raycast.get_collider()
+		if Input.is_action_just_released("Interact"):
+			if col.is_in_group("doors"):
+				col.open(global_position)
+				pass
+	else:
+		$UI/Center/Crosshair.rotation = 0
+
+func movement(delta: float) -> void:
 	# Gravity
 	if not is_on_floor():
 		velocity += GRAVITY * delta
-
+		
 	#  Jump
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI ac	tions with custom gameplay actions.
 	var input_dir := Input.get_vector("Left", "Right", "Forward", "Backward")
 	var direction := (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
@@ -55,7 +64,6 @@ func rotate_head(event: InputEventMouseMotion) -> void:
 	head.rotate_y(-event.relative.x * LOOK_SENSITIVITY)
 	camera.rotate_x(-event.relative.y * LOOK_SENSITIVITY)
 	camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(40))
-	print("gth")
 
 func toggle_mouse_mode() -> void:
 	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
